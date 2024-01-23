9d207abc4aad7dd3eba7826d2bd6273d1a4028b7
Nelly <nzental@gmail.com>
2024-01-23 18:31:32 +0100

    all new excersise

---
+++ scripts/fist table.R
@@ -0,0 +1,43 @@
+#dataframes
+#import
+download.file(url= "https://ndownloader.figshare.com/files/2292169",
+              destfile = "data_raw/portal_data_joined.csv")
+
+library(tidyverse)
+
+
+surveys <- read_csv("data_raw/portal_data_joined.csv")
+
+head(surveys) 
+view(surveys)
+str(surveys)
+dim(surveys)
+nrow(surveys)
+ncol(surveys)
+tail(surveys)
+colnames(surveys)
+summary(surveys)
+
+# indexing & subsetting 
+surveys[1, 6]
+
+surveys[1, ]
+surveys[ ,1]
+
+surveys[c(1,2,3), c(5,6)]
+surveys[1:4, 5:9]
+
+surveys[, -1&-5]
+
+surveys$weight
+
+#challange
+surveys_200 <- read_csv("data_raw/portal_data_joined.csv")
+surveys_200 <- surveys_200[200, ]
+surveys_200
+
+summary(surveys_200)
+
+nrow(surveys)
+surveys[nrow(surveys)/2, ]
+
