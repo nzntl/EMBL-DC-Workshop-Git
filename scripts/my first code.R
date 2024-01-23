97df290139c105ee2f6f77e4d115b2f6d392364d
Nelly <nzental@gmail.com>
2024-01-23 15:01:40 +0100

    this is my first r script

---
+++ scripts/my first code.R
@@ -0,0 +1,20 @@
+3 + 4
+
+weight_kg <- 55
+ # Alt -
+
+x2 <- 3
+Weights_kg <- 34
+
+(my_weight_kg <- 50)
+my_weight_kg
+
+2.2*weight_kg
+
+weight_kg <- 57
+2.2*weight_kg
+
+weight_lb <- 2.2*weight_kg
+weight_kg <- 100
+weight_lb
+
