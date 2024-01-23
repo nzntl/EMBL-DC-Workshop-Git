be3228b177960658d7937a90d1c2eff09358fe93
Nelly <nzental@gmail.com>
2024-01-23 17:23:23 +0100

    puh, a lot happened

--- scripts/my first code.R
+++ scripts/my first code.R
@@ -1,20 +1,27 @@
-3 + 4
+# %in%
+animals %in% c("rat", "frog", "cat", "duck", "dog")
+c("rat", "frog", "cat", "duck", "dog") %in% animals 
+animals[animals %in% c("rat", "frog", "cat", "duck", "dog")]
+animals[c("rat", "frog", "cat", "duck", "dog") %in% animals]
 
-weight_kg <- 55
- # Alt -
 
-x2 <- 3
-Weights_kg <- 34
+heights <- c(2, 4, 4, NA, 6)
+mean(heights)
+mean(heights, na.rm=T)
+max(heights, na.rm=T)
 
-(my_weight_kg <- 50)
-my_weight_kg
+is.na(heights)
+heights[!is.na(heights)]
 
-2.2*weight_kg
+na.omit(heights)
 
-weight_kg <- 57
-2.2*weight_kg
+heights[complete.cases(heights)]
 
-weight_lb <- 2.2*weight_kg
-weight_kg <- 100
-weight_lb
+heights <- c(63, 69, 60, 65, NA, 68, 61, 70, 61, 59, 64, 69, 63)
+mean(heights)
+heights_no_na <- na.omit(heights)
+heights_no_na <- heights[!is.na(heights)]
 
+median (heights, na.rm=T)
+heights_no_na[heights_no_na>67]
+length(heights_no_na[heights_no_na>67])
