917736fe2b56d09c78015df3163068580b1af2db
Nelly <nzental@gmail.com>
2024-01-24 10:07:49 +0100

    challanges

--- scripts/fist table.R
+++ scripts/fist table.R
@@ -45,3 +45,59 @@ my_list <- list(names = c("nelly", "jan"),
                 money = c(1, 2, 3, 4, 5, 6))
 
 my_list [[1]]
+
+str(surveys)
+
+surveys$sex <- factor(surveys$sex)
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels= c("male", "female"))
+
+#challange
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+# why do both work??
+nlevels(genus)
+nlevels(surveys$genus)
+
+levels(taxa)
+levels(surveys$taxa)
+
+sum(surveys$taxa == "Rabbit")
+sum(taxa == "Rabbit")
+
+summary(surveys)
+summary(surveys$taxa)
+
+
+#factor
+as.character(sex)
+
+#i dont get it
+year_fct <- factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+
+#renaming factors
+plot(surveys$sex)
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+levels(sex)[3] <- "undetermined"
+
+plot(sex)
+
+#challange2
+levels(sex)[1:2] <- c("female", "male")
+
+sex <- factor(sex, levels = c("undetermined", "female", "male"))
+plot(sex)
+              
